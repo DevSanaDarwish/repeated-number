1778f948743f74d90253b51a8354967cc0b4f045
sana darwish <Dev.Sanadar@gmail.com>
2024-09-22 01:31:29 +0300

    chore(setup): initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace RepeatedNumber
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.flpRandomLabels = new System.Windows.Forms.FlowLayoutPanel();
+            this.flpRandomLabels2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.btnSet = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.AutoSize = true;
+            this.lblQuestion.BackColor = System.Drawing.Color.Black;
+            this.lblQuestion.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.ForeColor = System.Drawing.Color.White;
+            this.lblQuestion.Location = new System.Drawing.Point(18, 46);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(576, 28);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "How many times is this number repeated in this matrix?";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Controls.Add(this.lblNumber);
+            this.panel1.Controls.Add(this.flpRandomLabels);
+            this.panel1.Controls.Add(this.lblQuestion);
+            this.panel1.Location = new System.Drawing.Point(1, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(806, 171);
+            this.panel1.TabIndex = 1;
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.BackColor = System.Drawing.Color.Black;
+            this.lblNumber.Font = new System.Drawing.Font("Tahoma", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumber.ForeColor = System.Drawing.Color.White;
+            this.lblNumber.Location = new System.Drawing.Point(391, 102);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(0, 40);
+            this.lblNumber.TabIndex = 1;
+            // 
+            // flpRandomLabels
+            // 
+            this.flpRandomLabels.Location = new System.Drawing.Point(0, 163);
+            this.flpRandomLabels.Name = "flpRandomLabels";
+            this.flpRandomLabels.Size = new System.Drawing.Size(806, 506);
+            this.flpRandomLabels.TabIndex = 39;
+            // 
+            // flpRandomLabels2
+            // 
+            this.flpRandomLabels2.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.flpRandomLabels2.Location = new System.Drawing.Point(185, 190);
+            this.flpRandomLabels2.Name = "flpRandomLabels2";
+            this.flpRandomLabels2.Size = new System.Drawing.Size(435, 331);
+            this.flpRandomLabels2.TabIndex = 2;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCheck.ForeColor = System.Drawing.Color.Indigo;
+            this.btnCheck.Location = new System.Drawing.Point(526, 548);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(94, 37);
+            this.btnCheck.TabIndex = 4;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // btnSet
+            // 
+            this.btnSet.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSet.ForeColor = System.Drawing.Color.Indigo;
+            this.btnSet.Location = new System.Drawing.Point(185, 548);
+            this.btnSet.Name = "btnSet";
+            this.btnSet.Size = new System.Drawing.Size(94, 37);
+            this.btnSet.TabIndex = 5;
+            this.btnSet.Text = "Set";
+            this.btnSet.UseVisualStyleBackColor = true;
+            this.btnSet.Click += new System.EventHandler(this.btnSet_Click);
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.ForeColor = System.Drawing.Color.White;
+            this.lblAnswer.Location = new System.Drawing.Point(72, 616);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(101, 28);
+            this.lblAnswer.TabIndex = 6;
+            this.lblAnswer.Text = "Answer: ";
+            // 
+            // txtResult
+            // 
+            this.txtResult.Font = new System.Drawing.Font("Tahoma", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResult.Location = new System.Drawing.Point(179, 617);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(100, 29);
+            this.txtResult.TabIndex = 7;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Font = new System.Drawing.Font("Tahoma", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResult.ForeColor = System.Drawing.Color.Red;
+            this.lblResult.Location = new System.Drawing.Point(43, 237);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(0, 28);
+            this.lblResult.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DarkSlateBlue;
+            this.ClientSize = new System.Drawing.Size(806, 681);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnSet);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.flpRandomLabels2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.FlowLayoutPanel flpRandomLabels;
+        private System.Windows.Forms.FlowLayoutPanel flpRandomLabels2;
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.Button btnSet;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.Label lblResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RepeatedNumber
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Random _rnd = new Random();
+
+        private byte _counter = 0;
+       
+        private void CreateLabel(System.Windows.Forms.Label lbl1)
+        {
+            lbl1.ForeColor = Color.White;
+            lbl1.BackColor = Color.Black;
+            lbl1.Size = new System.Drawing.Size(68, 54);
+            lbl1.Font = new Font("Tahoma", 22, FontStyle.Regular);
+            lbl1.Text = _rnd.Next(0, 10).ToString();
+            lbl1.TextAlign = ContentAlignment.MiddleCenter;
+            lbl1.BorderStyle = BorderStyle.FixedSingle;
+
+            flpRandomLabels2.FlowDirection = FlowDirection.LeftToRight;
+            
+            flpRandomLabels2.Controls.Add(lbl1);
+        }
+
+        private void AddLabelsInflp()
+        {
+            for (byte i = 0; i < 25; i++)
+            {
+                System.Windows.Forms.Label lbl1 = new System.Windows.Forms.Label();
+
+                CreateLabel(lbl1);
+
+                if (lbl1.Text == lblNumber.Text)
+                    _counter++;
+            }
+        }
+
+        private void btnSet_Click(object sender, EventArgs e)
+        {
+            _counter = 0;
+            txtResult.Text = "";
+            lblResult.Visible = false;
+            flpRandomLabels2.Controls.Clear();
+  
+            byte RandomNumber = Convert.ToByte(_rnd.Next(0, 10));
+
+            lblNumber.Text = RandomNumber.ToString();
+
+            AddLabelsInflp();
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            lblResult.Visible = true;
+
+            if (txtResult.Text == _counter.ToString())
+            {
+                lblResult.Text = "Correct :-)";
+                lblResult.ForeColor = Color.LimeGreen;
+            }
+
+            else
+            {
+                lblResult.Text = "Wrong :-(";
+                lblResult.ForeColor = Color.Red;
+            }
+
+        }
+    }
+}
